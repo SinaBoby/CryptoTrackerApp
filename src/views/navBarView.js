e5dd5e895f2a80b2001aba2b8d169fd1ad7bb3ed
SinaBoby <sinaboby91@yahoo.com>
2022-02-22 04:33:25 +0100

    logo src changed to github url

--- src/views/navBarView.js
+++ src/views/navBarView.js
@@ -1,7 +1,7 @@
 export const createNavBar = () => {
   const navBar = document.createElement('nav');
   navBar.innerHTML = String.raw`
-  <img src="../public/assets/logo2.png" alt="logo" id="logo">
+  <img src="https://github.com/SinaBoby/CryptoTrackerApp/blob/loading_spin_feature/public/assets/logo2.png?raw=true" alt="logo" id="logo">
   <ul id="nav-list">
   <li class="nav-item" id="market-page"><a>Market Meta Data</a></li>
   <li class="nav-item" id="technical-page"><a>Technical charts</a></li>
