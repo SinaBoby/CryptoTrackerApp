74db8b67e284f9d7d1604ccb6ccf68d8283388d4
SinaBoby <sinaboby91@yahoo.com>
2022-03-23 20:03:56 +0100

    hostname added

--- src/elementViews/loadLivePrice.js
+++ src/elementViews/loadLivePrice.js
@@ -17,7 +17,7 @@ function loadPriceTicker(symbol = 'btcusdt') {
   return new Promise((resolve, reject) => {
     let ws = new WebSocket(
       /* `wss://stream.binance.com:9443/stream?streams=${symbol}@miniTicker` */
-      `ws://${location.host}/live?symbol=${symbol}`,
+      `ws://${location.hostname}/live?symbol=${symbol}`,
     );
 
     ws.onopen = function () {
