9977f5cd26f354eab3cb643d3548823dbf7f80cf
SinaBoby <sinaboby91@yahoo.com>
2022-03-23 01:46:08 +0100

    ProcFile added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import app from './index.js';
 import queryString from 'query-string';
 import WebSocket, { WebSocketServer } from 'ws';
 const PORT = process.env.PORT || 3000;
+console.log(process.env)
 const server = app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
